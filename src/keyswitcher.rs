588976721a978c101958f54a74a2770cc9e9354d
Michael Hadley <mikethadley@gmail.com>
2020-09-26 14:35:44 -0700

    Basic pass-through of key events

--- src/keyswitcher.rs
+++ src/keyswitcher.rs
@@ -1,23 +1,57 @@
-use evdev_rs::{enums::EventType, Device, InputEvent, ReadFlag, UInputDevice};
-use std::{convert::From, fmt::Debug, fs::File, io};
+use input_linux::{
+    EvdevHandle, Event, EventKind, EventTime, InputEvent, InputId, Key, KeyEvent, SynchronizeEvent,
+    UInputHandle,
+};
+use libc::{input_event, timeval};
+use std::{
+    convert::{AsRef, From, TryInto},
+    fmt::Debug,
+    fs::{self, File},
+    io,
+    path::Path,
+    time::{SystemTime, SystemTimeError},
+};
+
+/// Taken from <linux/input.h>
+const BUS_USB: u16 = 0x03;
+
+/// Virtual Device info
+const VENDOR: u16 = 0x3232;
+const VERSION: u16 = 0x1234;
+const PRODUCT: u16 = 0x5678;
 
 pub struct Keyswitcher {
-    input_device: Device,
-    output_device: UInputDevice,
+    input_device: EvdevHandle<File>,
+    output_device: UInputHandle<File>,
 }
 
 impl Keyswitcher {
-    pub fn new(fd: File) -> Result<Self, Error> {
-        let input_device = Device::new_from_fd(fd).map_err(|e| Error::NoInputDeviceError(e))?;
+    pub fn new(input_path: impl AsRef<Path>) -> Result<Self, Error> {
+        let input_file = File::open(input_path)?;
+        let input_device = EvdevHandle::new(input_file);
+        input_device.grab(true)?;
 
-        let output_spec = Device::new().ok_or(Error::NoOutputDeviceSpec)?;
-        output_spec.set_name("Keyswitcher Virtual Input");
-        output_spec.enable(&EventType::EV_KEY)?;
+        let uinput = fs::OpenOptions::new().write(true).open("/dev/uinput")?;
+        let output_device = UInputHandle::new(uinput);
 
-        let output_device = UInputDevice::create_from_device(&output_spec)?;
+        output_device.set_evbit(EventKind::Key)?;
+        output_device.set_evbit(EventKind::Synchronize)?;
 
-        println!("{:?}", output_device.devnode());
-        println!("{:?}", output_device.syspath());
+        for key in Key::iter() {
+            output_device.set_keybit(key)?;
+        }
+
+        output_device.create(
+            &InputId {
+                bustype: BUS_USB,
+                vendor: VENDOR,
+                product: PRODUCT,
+                version: VERSION,
+            },
+            "Keyswitcher Virtual Input".as_bytes(),
+            0,
+            &[],
+        )?;
 
         Ok(Keyswitcher {
             input_device,
@@ -26,34 +60,88 @@ impl Keyswitcher {
     }
 
     pub fn run(&self) -> Result<(), Error> {
-        loop {
-            if let Ok((_read_status, event)) = self
-                .input_device
-                .next_event(ReadFlag::NORMAL | ReadFlag::BLOCKING)
-            {
-                self.handle_event(event)?;
+        //  Temporary limit on processed events, just in-case I lock up
+        //  my keyboard while working on this thing.
+        for _ in 0..50 {
+            // Initialize empty input_event buffer
+            let mut raw_events = [input_event {
+                time: timeval {
+                    tv_sec: 0,
+                    tv_usec: 0,
+                },
+                type_: 0,
+                code: 0,
+                value: 0,
+            }; 24];
+
+            let len = self.input_device.read(&mut raw_events)?;
+
+            for raw_event in raw_events.iter().take(len) {
+                let event = InputEvent::from_raw(raw_event)?.to_owned();
+                self.forward_event(event)?;
             }
         }
-    }
 
-    fn handle_event(&self, event: InputEvent) -> Result<(), Error> {
-        println!("{:?}", event.event_code);
-
-        self.output_device.write_event(&event)?;
         Ok(())
     }
+
+    fn forward_event(&self, event: InputEvent) -> Result<(), Error> {
+        if let Ok(Event::Key(key_event)) = Event::new(event) {
+            // TODO: Add the actual remapping logic here.
+            let events: [input_event; 2] = [
+                InputEvent::from(KeyEvent::new(
+                    get_timestamp()?,
+                    key_event.key,
+                    key_event.value,
+                ))
+                .as_raw()
+                .to_owned(),
+                InputEvent::from(SynchronizeEvent::report(get_timestamp()?))
+                    .as_raw()
+                    .to_owned(),
+            ];
+
+            self.output_device.write(&events)?;
+        }
+
+        Ok(())
+    }
+}
+
+fn get_timestamp() -> Result<EventTime, Error> {
+    let time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH)?;
+
+    Ok(EventTime::new(
+        (time.as_secs() as u64)
+            .try_into()
+            .map_err(|_| Error::SystemTimeError)?,
+        (time.subsec_micros() as u64)
+            .try_into()
+            .map_err(|_| Error::SystemTimeError)?,
+    ))
 }
 
 #[derive(Debug)]
 pub enum Error {
     IOError(io::Error),
-    NoInputDeviceError(io::Error),
-    NoOutputDeviceSpec,
-    NoOutputDeviceError(io::Error),
+    InputEventRangeError,
+    SystemTimeError,
 }
 
 impl From<io::Error> for Error {
     fn from(io_error: io::Error) -> Self {
         Error::IOError(io_error)
+    }
+}
+
+impl From<input_linux::RangeError> for Error {
+    fn from(_: input_linux::RangeError) -> Self {
+        Error::InputEventRangeError
+    }
+}
+
+impl From<SystemTimeError> for Error {
+    fn from(_: SystemTimeError) -> Self {
+        Error::SystemTimeError
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 use keyswitch::keyswitcher::{self, Keyswitcher};
-use std::{env, fs::File};
+use std::env;
 
 fn main() -> Result<(), keyswitcher::Error> {
     let device_name = env::args().nth(1).expect("Expected device name.");
-    let fd = File::open(&device_name)?;
-
-    Keyswitcher::new(fd)?.run()?;
+    Keyswitcher::new(device_name)?.run()?;
 
     Ok(())
 }
