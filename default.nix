cd6ed8816a225fb6a3a9f0e754104f3138515b6a
Michael Hadley <mikethadley@gmail.com>
2020-09-26 14:51:46 -0700

    Update nix build

--- default.nix
+++ default.nix
@@ -6,19 +6,9 @@ pkgs.rustPlatform.buildRustPackage rec {
 
   src = ./.;
 
-  cargoSha256 = "1j86p6zv7r809v05brid3j30zaxl8jvf76q70xz66g47ci4pckw8";
+  cargoSha256 = "0am5dw8pkspnrd5nvhbs7lz3aazr33cgf0dax5ybd7hr9gbf0bs3";
 
   verifyCargoDeps = true;
-
-  nativeBuildInputs = with pkgs; [
-    # Stuff for evdev-rs
-    python3
-    libtool
-    pkgconfig
-    autoconf
-    automake
-    libevdev
-  ];
 
   meta = with pkgs.stdenv.lib; {
     description = "Remap keys";
